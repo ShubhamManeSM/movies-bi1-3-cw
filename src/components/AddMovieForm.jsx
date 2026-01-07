21a338e50e8fdf410889dbc1e5839efcafb99e9d
Shubham Mane <shubhammane7096@gmail.com>
2026-01-07 16:29:14 +0530

    Initial commit

--- src/components/AddMovieForm.jsx
+++ src/components/AddMovieForm.jsx
@@ -27,7 +27,7 @@ const AddMovieForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault()
     try {
-      const response = await fetch("https://movies-api-brown-two.vercel.app/", 
+      const response = await fetch("https://movies-api-brown-two.vercel.app/movies", 
         {
           method: "POST",
           headers: {
